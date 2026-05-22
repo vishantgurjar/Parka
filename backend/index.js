a813def7ad4ed40d944a82ce59f96f4ff671bc9e
VISHANT PANWAR <vishantpanwar@example.com>
2026-05-22 21:25:56 +0530

    Replace bcrypt with bcryptjs to fix compilation crash on Vercel Serverless

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const Mechanic = require('./models/Mechanic');
